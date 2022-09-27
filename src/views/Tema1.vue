4574dd2895a4340c4a082251a0816bd914fd11ea
yisseth28 <yisseth28@hotmail.com>
2022-09-27 18:11:28 -0500

    git a la fecha

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -26,15 +26,15 @@
       .col-12
         img(src='@/assets/curso/temas/t1/img-2.svg', alt='img2')
         .mt-5
-
+        
     .bloque-texto-a.color-acento-contenido.p-4.p-md-5.mb-5 
       .row.m-0.align-items-center.justify-content-between
         .col-lg-4.mb-4.mb-lg-0
-          h2.mb-0 Según el Boletín Científico de las Ciencias Económico Administrativas del ICEA (Cruz García, 2019):  
+          h2.mb-0 Según el Boletín Científico de las Ciencias Económico Administrativas del ICEA (Cruz García, 2019): 
         .col-lg-8
-          .bloque-texto-a.color-acento-contenido__texto.p-4
+          .bloque-texto-a__texto.p-4
             p ‘Las fuentes de información son instrumentos para el conocimiento, acceso y búsqueda de la información, su objetivo principal es el de buscar, fijar y difundir la fuente de información implícita en cualquier soporte físico, estas se pueden catalogar desde diferentes perspectivas, sin embargo, cada autor puede elaborar su propia clasificación dependiendo su grado de información. De acuerdo con el grado de información que proporcionan, las fuentes de información se dividen en primarias, secundarias y terciarias; esta división se utiliza generalmente en el ámbito académico.’ (p.57)
-            
+
     p.mb-5 Las fuentes de información también son una herramienta para el razonamiento, análisis, búsqueda y entrada de información, como punto de partida para generación de hipótesis, conclusiones, informes, que pueden llevar a la solución de problemas o exploración de respuestas organizacionales. La selección de las mismas dependerá de la disponibilidad y el acceso de la información. 
     p.mb-5 Esto requiere un proceso objetivo y consecuente con los resultados que se estiman obtener. Es por ello necesario considerar sus características para lograr la mayor adaptación en los resultados. 
 
@@ -57,7 +57,7 @@
         h4.text-center Para reunir datos primarios, es importante establecer tres elementos: identificar los métodos e instrumentos de investigación, elaborar un plan de muestreo y definir las técnicas para establecer contacto con la muestra.
     .row.mt-5
         .col-12 
-        img(src='@/assets/curso/temas/t1/img-4.svg' alt='img-4')
+        img(src='@/assets/curso/temas/t1/img-4.png' alt='img-4')
     .row.mt-5
     .cajon-b.color-secundario.p-3.mb-5
       br
@@ -77,6 +77,9 @@
 
     .row.mt-5
       .col-6
+        img(src='@/assets/curso/temas/t1/img-7.png' alt='img-7')
+      .col-6
+        p.mb-5 <b>Las fuentes primarias pueden presentarse a traves de:</b>
         AcordionA(tipo="b" clase-tarjeta="tarjeta tarjeta--azul")
           .row(titulo="Monografías")
             .col-md-6.mb-4.mb-md-0
@@ -97,13 +100,72 @@
           .row(titulo="Encuesta")
             .col-md-6.mb-4.mb-md-0
               p Es un instrumento en la cual se registran situaciones que pueden ser observadas o se cuestiona a la persona seleccionada para el estudio. La encuesta es un método descriptivo en el que se detectan ideas, necesidades, gustos, hábitos de uso, preferencias, etc.  Utilizando procedimientos estructurados de interrogación en aras de obtener mediciones cuantitativas partiendo de la variedad de variables definidas. Estos datos posteriormente podrán ser clasificados, interpretados y analizados.
-              br
-              p Las características de una encuesta son:
-              p La encuesta representa una observación indirecta de la realidad por medio de lo que manifiestan los encuestados.
-                ul
-                li Es un método diseñado para una investigación específica.
-                li Se adapta a una aplicación masiva a través de un sistema de muestreo
-                li Es posible que la aplicación llegue a la subjetividad de la población seleccionada, lo que permitirá mayor precisión en la información obtenida 
+              p <b>Las características de una encuesta son:</b>
+                ul.lista-ul.mb-3
+                  li 
+                    i.fas.fa-angle-right
+                      |La encuesta representa una observación indirecta de la realidad por medio de lo que manifiestan los encuestados.
+                  li
+                  li 
+                    i.fas.fa-angle-right
+                      |Es un método diseñado para una investigación específica
+                  li
+                  li 
+                    i.fas.fa-angle-right
+                      |Se adapta a una aplicación masiva a través de un sistema de muestreo.
+                  li
+                  li 
+                    i.fas.fa-angle-right
+                      |Es posible que la aplicación llegue a la subjetividad de la población seleccionada, lo que permitirá mayor precisión en la información obtenida.
+                  li
+            P <b>Las encuestas se pueden desarrollar de las siguientes formas:</b>
+              ol.lista-ol
+                li 
+                  span.text-bold 1. 
+                    |Entrevista personal
+                li
+                li 
+                  span.text-bold 1. 
+                    |Entrevista por teléfono
+                li
+                li 
+                  span.text-bold 1. 
+                    |Cuestionario por correo
+                li
+                li 
+                  span.text-bold 1. 
+                    |Observación directa.
+                li
+                li 
+                  span.text-bold 1. 
+                    |Vía electrónica.
+                li
+            
+    .titulo-segundo.color-secundario.mb-5        
+      h2 1.2 Información secundaria
+      p Las fuentes secundarias son aquellas que dependen de información existente, como pueden ser datos obtenidos de anuarios estadísticos, de internet, de medios de comunicación, monografías, etc.
+      p Como complemento a esta definición la  Universidad Autónoma del estado de Hidalgo, (2008) afirma en su documento ‘Fuentes de Información’ que “Este tipo de fuentes son las que ya han procesado información de una fuente primaria. El proceso de esta información se pudo dar por una interpretación, un análisis, así como la extracción y reorganización de la información de la fuente primaria” (p.3).
+    h2.text-center Ejemplos de fuentes secundarias
+    .row.mt-5
+      .col-12 
+        img(src='@/assets/curso/temas/t1/img-6.png' alt='img-6')
+    .row.mt-5
+      .col-7
+        img(src='@/assets/curso/temas/t1/img-8.png' alt='img-8')
+      .col-6
+    .cajon-b.color-secundario.p-3.mb-5
+      br
+      br
+      p.mb-0 Para emplear las fuentes de información se debe analizar los siguientes interrogantes básicos que son: 
+     
+      |✔	¿Es pertinente la información existente en la investigación? <br/>
+      |✔	¿La información se adapta a los objetivos establecidos en la investigación? <br/>
+      |✔	¿Está actualizada en relación a la vigencia del tema en estudio? <br/>
+      |✔	¿La veracidad de la fuente ha sido cuestionada por algún autor? <br/>
+      |✔	¿La información se obtuvo a través de metodologías adecuadas?<br/>
+      |✔	¿La información compromete a un público específico?<br/>
+      |✔	¿La información obtenida es ética?<br/>
+      |✔	¿Guarda objetividad?<br/>
 </template>
 
 <script>
--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -5,7 +5,20 @@
     .titulo-principal.color-acento-contenido
       .titulo-principal__numero
         span 2
-      h1 Titulo de primer nivel
+      h1 Tipos de datos
+    .bloque-texto-a.color-secundario.p-4.p-md-5.mb-5 
+      .row.m-0.align-items-center.justify-content-between
+        .col-lg-4.mb-4.mb-lg-0
+          img(src='@/assets/curso/temas/t2/img-1.png' alt='img-1')
+        .col-lg-8
+          .bloque-texto-a__texto.p-4
+            p En el proceso de recolección de datos se debe establecer el tipo de información que se quiere estudiar, dependiendo de los resultados que se esperan recoger. Esta decisión está relacionada directamente con los objetivos propuestos desde un inicio en la investigación. Los tipos de datos más utilizados son: los textuales, conocidos como datos cualitativos, y los numéricos, llamados datos cuantitativos.
+    .bloque-texto-b.color-secundario.p-4
+      .bloque-texto-b__texto
+        i.fas.fa-quote-left
+          h2.mb-0 Can we all just agree as the greater design community to stop talking about Comic Sans altogether?
+        i.fas.fa-quote-right
+    h2 1.1  Cualitativos
 
 </template>
 
