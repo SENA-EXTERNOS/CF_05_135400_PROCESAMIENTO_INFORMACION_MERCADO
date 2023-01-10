ade50c609602bb65689e56489196739ea09ff748
payepesch <payepesch@gmail.com>
2023-01-10 16:28:22 -0500

    Ajustes 2

--- src/config/global.js
+++ src/config/global.js
@@ -126,53 +126,50 @@ export default {
   },
   complementario: [
     {
-      tema: 'Fuentes de información (1)',
+      tema: 'Fuentes de información',
       referencia:
-        'Margarita Aida Cruz García. (2019). <i>Boletín Científico de las Ciencias Económico Administrativas del ICEA.</i> ',
+        'Cruz García, M. M. (2019). <em>Boletín Científico de las Ciencias Económico Administrativas del ICEA</em>.',
       tipo: 'Artículo',
       link:
         'https://repository.uaeh.edu.mx/revistas/index.php/icea/article/view/4864/6748',
     },
     {
-      tema: 'Fuentes de información (2)',
+      tema: 'Fuentes de información',
       referencia:
-        'MCE. Marisol Maranto Rivera ETE. María Eugenia González Fernández (2015). Fuentes de información. Universidad Autónoma del estado de Hidalgo.',
+        'Maranto Rivera, M. y  González Fernández, M. E. (2015). <em>Fuentes de información</em>. Universidad Autónoma del estado de Hidalgo.',
       tipo: 'Boletín virtual',
       link:
         'https://repository.uaeh.edu.mx/bitstream/bitstream/handle/123456789/16700/LECT132.pdf',
     },
     {
-      tema: 'Métodos de recolección de datos para una investigación.',
+      tema: 'Fuentes de información',
       referencia:
-        'Mariela Torres, K. P. (s.f.). Métodos de recolección de datos para una investigación. Boletín electrónico No. 03. Facultad de Ingeniería, Universidad Rafael Landívar.',
+        'Rodríguez, I. V. (2008). <em>Metodología para la elaboración de guías de fuentes de información</em>. Universidad Complutense de Madrid.',
+      tipo: 'Artículo',
+      link: 'https://www.scielo.org.mx/pdf/ib/v22n46/v22n46a6.pdf',
+    },
+    {
+      tema: 'Fuentes de información',
+      referencia:
+        'Escuela de Administración de Negocios. (2008). <em>Metodologías de la Investigación en las Ciencias Sociales</em>. Fases, fuentes y selección de técnicas. Revista EAN.',
+      tipo: 'Artículo',
+      link: 'https://www.redalyc.org/pdf/206/20612981002.pdf',
+    },
+    {
+      tema: 'Tipos de datos',
+      referencia:
+        'Torres, M. (s.f.). <em>Métodos de recolección de datos para una investigación</em>. Boletín electrónico No. 03. Facultad de Ingeniería, Universidad Rafael Landívar.',
       tipo: 'Boletín',
       link:
         'http://148.202.167.116:8080/jspui/bitstream/123456789/2817/1/M%c3%a9todos%20de%20recolecci%c3%b3n%20de%20datos%20para%20una%20investigaci%c3%b3n.pdf',
     },
     {
-      tema:
-        'Metodología para la elaboración de guías de fuentes de información',
+      tema: 'Seguridad de los datos',
       referencia:
-        'Rodríguez, I. V. (2008). Metodología para la elaboración de guías de fuentes de información. México: Universidad Complutense de Madrid.',
-      tipo: 'Artículo',
-      link:
-        'https://repository.uaeh.edu.mx/bitstream/bitstream/handle/123456789/16700/LECT132.pdf',
-    },
-    {
-      tema: 'Guía de implementación de seguridad de la información',
-      referencia:
-        'ISO 27001. (2013). Guía de implementación de seguridad de la información. Obtenido de https://www.nqa.com/medialibraries/NQA/NQA-Media-Library/PDFs/Spanish%20QRFs%20and%20PDFs/NQA-ISO-27001-Guia-de-implantacion.pdf ',
+        'ISO 27001. (2013). <em>Guía de implementación de seguridad de la información.</em>',
       tipo: 'Norma',
       link:
         'https://www.nqa.com/medialibraries/NQA/NQA-Media-Library/PDFs/Spanish%20QRFs%20and%20PDFs/NQA-ISO-27001-Guia-de-implantacion.pdf',
-    },
-    {
-      tema:
-        'Metodologías de la Investigación en Ciencias Sociales. Fases, fuentes y selección de técnicas',
-      referencia:
-        'Escuela de Administración de Negocios. (2008). Metodologías de la Investigación en las Ciencias Sociales. Fases, fuentes y selección de técnicas (pág. 9). Revista EAN',
-      tipo: 'Artículo',
-      link: 'https://www.redalyc.org/pdf/206/20612981002.pdf',
     },
   ],
   glosario: [
@@ -244,33 +241,12 @@ export default {
   referencias: [
     {
       referencia:
-        'Alfredo Baronio, S. C. (2018). Saber y Saber Hacer con Estadística. Córdoba, Argentina: UniRio.',
+        'Baronio, A. (2018). <em>Saber y Saber Hacer con Estadística</em>. UniRio.',
       link: '',
     },
     {
       referencia:
-        'Escuela de Administración de Negocios. (2008). Metodologías de la Investigación en Ciencias Sociales. En C. L. Ibáñez, Fases, fuentes y selección de técnicas (pág. 9). Revista EAN.',
-      link: '',
-    },
-    {
-      referencia:
-        'ISO 27001. (2013). Sistema de Gestión de Seguridad en la Información.',
-      link: '',
-    },
-    {
-      referencia:
-        'Cruz García, M. A. (2019). Boletín Científico de las Ciencias Económico Administrativas del ICEA. Obtenido de ',
-      link:
-        'https://repository.uaeh.edu.mx/revistas/index.php/icea/article/view/4864/6748 ',
-    },
-    {
-      referencia:
-        'Mariela Torres, K. P. (s.f.). MÉTODOS DE RECOLECCIÓN DE DATOS PARA UNA INVESTIGACIÓN. Boletín electrónico No. 03. Facultad de Ingeniería, Universidad Rafael Landívar.',
-      link: '',
-    },
-    {
-      referencia:
-        'Rodríguez, I. V. (2008). Metodología para la elaboración de guías de fuentes de información. México: Universidad Complutense de Madrid.',
+        'ISO 27001. (2013). <em>Sistema de Gestión de Seguridad en la Información.</em>',
       link: '',
     },
   ],
